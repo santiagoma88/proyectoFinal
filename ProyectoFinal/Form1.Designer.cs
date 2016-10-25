587bacd398ea214a4422986df255eb1136e24042
santiago martinez <santiagoma88@gmail.com>
2016-10-25 00:43:20 -0500

    trabajando movimiento

--- ProyectoFinal/Form1.Designer.cs
+++ ProyectoFinal/Form1.Designer.cs
@@ -28,37 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ventana));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // player1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(88, 68);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.player1.BackColor = System.Drawing.Color.Transparent;
+            this.player1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.player1.Image = global::ProyectoFinal.Properties.Resources.player11;
+            this.player1.Location = new System.Drawing.Point(226, 89);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(60, 90);
+            this.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // player2
+            // 
+            this.player2.Image = global::ProyectoFinal.Properties.Resources.player21;
+            this.player2.Location = new System.Drawing.Point(226, 239);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(60, 90);
+            this.player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player2.TabIndex = 1;
+            this.player2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // ventana
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1060, 493);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1059, 523);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "ventana";
-            this.Text = "Peace Runner";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "Peace Runner!!!";
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ProyectoFinal/Form1.cs
+++ ProyectoFinal/Form1.cs
@@ -11,9 +11,28 @@ namespace ProyectoFinal
 {
     public partial class ventana : Form
     {
+        int contadorMovimiento = 1;
         public ventana()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            int cantidadMovimiento = 5;
+            if(contadorMovimiento <= cantidadMovimiento)
+            {
+                player1.Image = Properties.Resources.player12;
+                contadorMovimiento++;
+            }
+            if ((contadorMovimiento > cantidadMovimiento / 2) && (contadorMovimiento <= cantidadMovimiento * 2)) 
+            {
+                player1.Image = Properties.Resources.player11;
+                contadorMovimiento++;
+            }
+
+            contadorMovimiento = (contadorMovimiento > cantidadMovimiento *2 ) ? 0 : contadorMovimiento;
         }
     }
 }
